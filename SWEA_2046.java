f89ff84bc57ac93b748e430b75ee515d13c79179
sairo415 <103760413+sairo415@users.noreply.github.com>
2022-07-15 17:13:28 +0900

    Add files via upload

---
+++ SWEA_2046.java
@@ -0,0 +1,17 @@
+package 알고리즘문제풀이;
+
+import java.util.Scanner;
+
+public class SWEA_2046 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.print("#");			
+		}
+		sc.close();
+	}
+
+}
